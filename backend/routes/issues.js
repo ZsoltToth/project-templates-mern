3aa5a5291a022265abe9f5983f454fbdfb3d285b
Zsolt Toth <tothzs87@gmail.com>
2021-01-03 16:09:10 +0100

    chore(1): swagger was configured

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -3,11 +3,33 @@ const router = express.Router()
 const issuesController = require('../controller/issues')
 const issueRequestDto = require('./dto/issueRecordRequestDto')
 
-/* GET home page. */
+/**
+ * @swagger
+ * /issues:
+ *  get:
+ *      summary: Fetches all issues
+ *      responses:
+ *          200:
+ *              description: list of issues
+ */
 router.get('/', issuesController.readIssue)
 
+/**
+ * @swagger
+ * /issues/{id}:
+ *      get:
+ *          summary: get issue by id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: integer
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: a single issue object
+ *
+ */
 router.get('/:id', issuesController.readIssue)
-
 router.post('/', issueRequestDto, issuesController.createIssue)
 
 module.exports = router
