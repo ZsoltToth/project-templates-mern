b988fcc895efd58be05c688a83dac01a3c878d61
Zsolt Toth <tothzs87@gmail.com>
2022-01-30 06:15:08 +0100

    feat(#36): add theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,35 @@ import Greetings from './component/Greetings';
 import Counter from './component/Counter';
 import IssueRecordingForm from './component/IssueRecordingForm';
 import IssueTable from './component/IssueTable';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Greetings name={'React'}/>
-        <Counter/>
-        <IssueRecordingForm/>
-        <IssueTable/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Greetings name={'React'}/>
+            <Counter/>
+            <IssueRecordingForm/>
+            <IssueTable/>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ frontend/src/component/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const Layout: FunctionComponent = () => {
+  return (
+    <>
+      <p>NavBar</p>
+      <p>Content</p>
+      <p>Footer</p>
+    </>
+  );
+};
